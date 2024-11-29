e463bd269b02eccd6983c44a85006b50c68ed8db
RahulYadav22775 <gopichand556677@gmail.com>
2024-11-29 15:44:40 +0530

    docker

--- installations.sh
+++ installations.sh
@@ -39,23 +39,23 @@ systemctl enable docker
 usermod -aG docker ec2-user
 VALIDATE $? "Docker installation"
 
-# eksctl
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-mv /tmp/eksctl /usr/local/bin
-VALIDATE $? "eksctl installation"
+# # eksctl
+# curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+# tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+# mv /tmp/eksctl /usr/local/bin
+# VALIDATE $? "eksctl installation"
 
 
-# kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mv kubectl /usr/local/bin/kubectl
-VALIDATE $? "kubectl installation"
+# # kubectl
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+# chmod +x ./kubectl
+# mv kubectl /usr/local/bin/kubectl
+# VALIDATE $? "kubectl installation"
 
-# kubens
-git clone https://github.com/ahmetb/kubectx /opt/kubectx
-ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-VALIDATE $? "kubens installation"
+# # kubens
+# git clone https://github.com/ahmetb/kubectx /opt/kubectx
+# ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+# VALIDATE $? "kubens installation"
 
 
 # Helm
