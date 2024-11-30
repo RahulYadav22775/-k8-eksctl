9c88a1586ae226c494423539c03be8c135361c87
RahulYadav22775 <gopichand556677@gmail.com>
2024-11-30 09:26:17 +0530

    docker

--- installations.sh
+++ installations.sh
@@ -49,7 +49,7 @@ VALIDATE $? "Docker installation"
 # # kubectl
 # curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
 # chmod +x ./kubectl
-# mv kubectl /usr/local/bin/kubectl
+# mv kubectl /usr/local/bin/
 # VALIDATE $? "kubectl installation"
 
 # # kubens
